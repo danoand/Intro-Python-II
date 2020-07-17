1cd1addd72ef71676fc48cdd43119aed457d9e5a
Dan Anderson <danfanderson@gmail.com>
2020-07-16 20:55:05 -0500

    Update adv.py

--- src/adv.py
+++ src/adv.py
@@ -6,7 +6,7 @@ from player import Player
 pp = pprint.PrettyPrinter(indent=2)
 
 # Declare all the rooms
-print("STARTUP: Hey... setting up some cool rooms...")
+print("Starting up... and setting up some cool rooms")
 room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
@@ -28,7 +28,7 @@ earlier adventurers. The only exit is to the south."""),
 
 
 # Link rooms together
-print("STARTUP: Hey... linking rooms together...")
+print("Now... linking rooms together\n\n")
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
@@ -45,10 +45,19 @@ room['treasure'].s_to = room['narrow']
 # pp.pprint(room)
 
 # Make a new player object that is currently in the 'outside' room.
-playa = Player("John", "outside")
-
+playa = Player("Johnny", "outside")
+game_input = None
 # Write a loop that:
 #
+while game_input != "q":
+    print("Player: {name} is currently in room: {room_current}".format(
+        name=playa.name,
+        room_current=room[playa.room_current].name
+    ))
+    print("({description})\n\n".format(description=room[playa.room_current].description))
+
+    game_input=input("What should {name} do now? ".format(name=playa.name))
+
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
@@ -57,3 +66,5 @@ playa = Player("John", "outside")
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+if game_input == "q":
+    print("Thanks for playing... quitting the game now.")
